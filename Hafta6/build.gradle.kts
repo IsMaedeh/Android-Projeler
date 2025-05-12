cf02121a48a8e60e54480c0fef9c9d4d266dd5cb
Maedeh <137514099+IsMaedeh@users.noreply.github.com>
2025-05-12 11:58:50 +0300

    Added Hafta6

---
+++ Hafta6/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+//plugins {
+//
+//    id("com.android.application") version "8.2.2" apply false
+//}
+//
+//    buildscript {
+//        repositories {
+//            google()
+//            mavenCentral()
+//        }
+//
+//        dependencies {
+//            classpath ("com.android.tools.build:gradle:7.0.4")
+//            classpath ("com.google.gms:google-services:4.4.1")
+//
+//        }
